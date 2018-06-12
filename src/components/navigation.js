57caca6a4adb6e72b5b5b84dae141807d29d55b5
Alexa Scott <arscott4792@gmail.com>
2018-06-12 19:14:04 -0400

    removed spans from nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -22,8 +22,8 @@ class Navigation extends React.Component {
 
             <nav className="navigation__nav">
                 <ul className="navigation__list">
-                    <li className="navigation__item"><a href="" className="navigation__link"><span>01</span>Home</a></li>
-                    <li className="navigation__item"><a href="" className="navigation__link"><span>02</span>Clients</a></li>
+                    <li className="navigation__item"><a href="" className="navigation__link">Home</a></li>
+                    <li className="navigation__item"><a href="" className="navigation__link">Clients</a></li>
                     <li className="navigation__item">
                         <a 
                           href="" 
@@ -31,7 +31,7 @@ class Navigation extends React.Component {
                             e.preventDefault();
                             this.logOut();
                             }}
-                          className="navigation__link"><span>03</span>Logout
+                          className="navigation__link">Logout
                         </a>
                     </li>
                 </ul>
