3bf48f9c5605bbbdcc1a089e1ac383a12ebce8c0
Joshua Hutchinson <jthutch94@gmail.com>
2018-06-12 12:41:33 -0400

    working on components

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import HeaderBar from './header-bar';
+import Calendar from 'react-calendar';
+import Schedule from './schedule';
 
 export default class Dashboard extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            date: new Date()
+        }
+    };
+
+    onChange = (date) => 
+        this.setState({ date });
+
     render() {
+        console.log(this.state);
         return (
             <div>
                 <HeaderBar />
-                This is the Dashboard
+                {/*This is the Dashboard*/}
+                <Calendar
+                    onChange={this.onChange}
+                    value={this.state.date}
+                />
+                <Schedule />
             </div>
         )
     }
