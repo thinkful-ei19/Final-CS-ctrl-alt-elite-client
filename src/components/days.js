3001b106b3edfeb2d0e21f5f683e839cfb5bbff9
Julie K <julieskim160@gmail.com>
2018-06-13 13:54:27 -0700

    capitalize days

--- src/components/days.js
+++ src/components/days.js
@@ -5,13 +5,13 @@ export default class Days extends React.Component {
     render() {
         return (
             <div className="calendar__days">
-                <span className="calendar__days__day">Sun</span>
-                <span className="calendar__days__day">Mon</span>
-                <span className="calendar__days__day">Tue</span>
-                <span className="calendar__days__day">Wed</span>
-                <span className="calendar__days__day">Thu</span>
-                <span className="calendar__days__day">Fri</span>
-                <span className="calendar__days__day">Sat</span>
+                <span className="calendar__days__day">SUN</span>
+                <span className="calendar__days__day">MON</span>
+                <span className="calendar__days__day">TUE</span>
+                <span className="calendar__days__day">WED</span>
+                <span className="calendar__days__day">THU</span>
+                <span className="calendar__days__day">FRI</span>
+                <span className="calendar__days__day">SAT</span>
             </div>
         )
     }
