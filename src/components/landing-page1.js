2f49cec62386c936e2f689d24670990e7a2c5b04
Julie K <julieskim160@gmail.com>
2018-06-15 09:07:09 -0700

    landing page

--- src/components/landing-page1.js
+++ src/components/landing-page1.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
+import background from '../media/pencil.jpg';
+
+
+const sectionStyle = {
+    backgroundSize: "100% 100%",
+    height: "100VH",
+    backgroundRepeat: "no-repeat",
+    backgroundImage: `url(${background})`
+};
 
 export function LandingPage1(props) {
 
     return(
-        <div className="landing-page">
-            <h1></h1>
-
-            <p>Let's get you started...</p>
-        </div>
-    
+        <section style={ sectionStyle }>
+            <div className="landing-page">
+                <h1>APP-NAME</h1>
+            
+                <p>a simple appointment scheduling app with 
+                    automated notifications</p>
+            </div>
+        </section>
     );
 }
