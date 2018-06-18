e5a4f83a0eec7cf8f8f294df137e757aa8d3403f
Julie K <julieskim160@gmail.com>
2018-06-18 14:50:48 -0700

    merge

--- src/components/reports.js
+++ src/components/reports.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 // import Chart from 'chart.js';
 import Navigation from './navigation';
 import LineGraph from './reportsLineGraph';
