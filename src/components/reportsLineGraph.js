97d267b074a715aadbe3631080dbb76119c6f80e
Julie K <julieskim160@gmail.com>
2018-06-18 13:18:07 -0700

    line graph

--- src/components/reportsLineGraph.js
+++ src/components/reportsLineGraph.js
@@ -3,25 +3,25 @@ import { connect } from 'react-redux';
 import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 const data = [
-    {name: 'January', uv:5},
-    {name: 'February', uv:4},
-    {name: 'March', uv:6},
-    {name: 'April', uv:14},
-    {name: 'May', uv:9},
-    {name: 'June', uv:4},
-    {name: 'July', uv:16}
+    {name: 'January', appointments:5},
+    {name: 'February', appointments:4},
+    {name: 'March', appointments:6},
+    {name: 'April', appointments:14},
+    {name: 'May', appointments:9},
+    {name: 'June', appointments:4},
+    {name: 'July', appointments:16}
 ]
 
 export default class LineGraph extends React.Component {
     render() {
     return(
-        <LineChart width={730} height={250} data={data}
+        <LineChart width={780} height={250} data={data}
         margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
         <XAxis dataKey="name" />
         <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="uv" stroke="#5DADE2" />
+        <Tooltip cursor={false}/>
+        <Legend verticalAlign="top" height={36} iconType='rect'/>
+        <Line type="monotoneX" dataKey="appointments" stroke="#5DADE2" dot={{ stroke: '#5DADE2', strokeWidth: .5 }}/>
         </LineChart>
     
     )} 
