38d07f0c120e2f262680e7306c28b627d4675663
Alexa Scott <arscott4792@gmail.com>
2018-06-19 14:25:40 -0400

    appointments rendering from line graph!

--- src/components/reportsLineGraph.js
+++ src/components/reportsLineGraph.js
@@ -4,11 +4,24 @@ import moment from 'moment';
 
 
 export default class LineGraph extends React.Component{
-    handleClick() {
-        console.log('hi');
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            appointments: ''
+        };
+    }
+    
+    handleClick(month) {
+        this.setState({
+            name: month,
+            appointments: this.state.appointments
+        });
     }
 
 	render () {
+           let apptInfo = [];
+           // console.log('typeof apptInfo:', typeof apptInfo);
            let janCount = 0;
            let febCount = 0;
            let marCount = 0;
@@ -24,10 +37,9 @@ export default class LineGraph extends React.Component{
         // console.log('APPT ARRAY', this.props.user.appointments);
         // console.log('PROPS', this.props.user.appointments.time);
        const totalAppointmentsForUser = this.props.user.appointments.length;
-       console.log('this is the total amount of appts for user', totalAppointmentsForUser);
+    //    console.log('this is the total amount of appts for user', totalAppointmentsForUser);
        const filterAppts = this.props.user.appointments.map((appointment) => {
            const formatTime = moment(appointment.time).format('MMMM Do YYYY');
-           // console.log(formatTime);
            if (formatTime.includes('January')) {
                 janCount ++;
            } if (formatTime.includes('February')) {
@@ -54,27 +66,62 @@ export default class LineGraph extends React.Component{
                 decCount ++;
             }           
        });
-       console.log(`1: ${janCount}, 2: ${febCount}, .... 6: ${juneCount}, 7: ${julyCount}, 8: ${augCount}, 12 ${decCount}`);
+    //    console.log(`1: ${janCount}, 2: ${febCount}, .... 6: ${juneCount}, 7: ${julyCount}, 8: ${augCount}, 12 ${decCount}`);
+       const filterApptList = this.props.user.appointments.map((appointment) => {
+        const formatTime = moment(appointment.time).format('MMMM');
+        const arrayOfTime = formatTime.split(' ');
+        
+        //console.log(arrayOfTime[0]);
+        if(arrayOfTime[0] === this.state.name) {
+            apptInfo.push(appointment);
+        }
+       });
+
+       console.log('apptinfo!!', apptInfo);
+       console.log('typeof apptInfo:', Array.isArray(apptInfo));
+       
+       const apptDataList = apptInfo.map((appt) => {
+           return (
+        //    <li>{appt.client}</li>
+        <li key={appt.id}>
+        Client Name: {appt.client.name}
+        Client Email: {appt.client.email}
+        Client Phone: {appt.client.phone}
+        Appt Notes: {appt.notes}
+        </li>
+           )
+       });
+
+       console.log(apptDataList);
+       
+       
+
        const data = [
-        {name: 'Jan', appointments: janCount},
-        {name: 'Feb', appointments: febCount},
+        {name: 'January', appointments: janCount},
+        {name: 'February', appointments: febCount},
         {name: 'March', appointments: marCount},
         {name: 'April', appointments: aprCount},
         {name: 'May', appointments: mayCount},
         {name: 'June', appointments: juneCount},
         {name: 'July', appointments: julyCount},
         {name: 'August', appointments: augCount},
-        {name: 'Sept', appointments: septCount},
-        {name: 'Oct', appointments: octCount},
-        {name: 'Nov', appointments: novCount},
-        {name: 'Dec', appointments: decCount}
+        {name: 'September', appointments: septCount},
+        {name: 'October', appointments: octCount},
+        {name: 'November', appointments: novCount},
+        {name: 'December', appointments: decCount}
     ]
+    // console.log('STATE', this.state);
        return(
+           <div>
+               <div>
             <LineChart 
                 width={780} 
                 height={250} 
                 data={data}
-                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                onClick={(e) => {
+                    this.handleClick(e.activeLabel)
+                }}>
             <XAxis dataKey="name" />
             <YAxis />
             <Tooltip cursor={false} />
@@ -87,12 +134,17 @@ export default class LineGraph extends React.Component{
                 type="monotoneX" 
                 dataKey="appointments" 
                 stroke="#5DADE2" 
-                onMouseDown={() => console.log('hi')}
                 dot={{ 
                     stroke: '#5DADE2', 
                     strokeWidth: .5
                 }}/>
             </LineChart>
+            </div>
+            <h1>{this.state.name}</h1>
+            <ul>
+                {apptDataList}
+            </ul>
+            </div>
         );
     }
 }
