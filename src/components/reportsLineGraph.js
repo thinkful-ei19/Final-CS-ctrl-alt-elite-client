b835cb0aa5b135bd30eb4a693fc5cc5c9fd1c2a2
Alexa Scott <arscott4792@gmail.com>
2018-06-19 16:16:32 -0400

    line graph and appt history rendering

--- src/components/reportsLineGraph.js
+++ src/components/reportsLineGraph.js
@@ -8,20 +8,21 @@ export default class LineGraph extends React.Component{
         super(props);
         this.state = {
             name: '',
-            appointments: ''
+            click: false,
+            apptMessage: ''
         };
     }
     
     handleClick(month) {
         this.setState({
             name: month,
-            appointments: this.state.appointments
+            click: true
         });
     }
 
 	render () {
            let apptInfo = [];
-           // console.log('typeof apptInfo:', typeof apptInfo);
+           
            let janCount = 0;
            let febCount = 0;
            let marCount = 0;
@@ -77,22 +78,32 @@ export default class LineGraph extends React.Component{
         }
        });
 
-       console.log('apptinfo!!', apptInfo);
-       console.log('typeof apptInfo:', Array.isArray(apptInfo));
+       // console.log('apptinfo!!', apptInfo);
+       
        
        const apptDataList = apptInfo.map((appt) => {
            return (
                 <li key={appt.id}>
-                Client Name: {appt.client.name}
-                Client Email: {appt.client.email}
-                Client Phone: {appt.client.phone}
-                Appt Notes: {appt.notes}
+                Date of Appt: {moment(appt.time).format('MMMM Do YYYY')} <br/>
+                Client Name: {appt.client.name} <br/>
+                Client Email: {appt.client.email} <br/>
+                Client Phone: {appt.client.phone} <br/>
+                Appt Notes: {appt.notes} <br /> <br />
                 </li>
            )
        });
 
-       console.log(apptDataList);
-       
+       // console.log('check it', apptDataList);
+
+       const handleMessage = () => {
+        if (apptDataList === []) {
+            this.setState({
+                apptMessage: 'There were no appointments this month.'
+            })
+           }
+    }
+
+    // console.log('message', this.state.apptMessage);
        
 
        const data = [
@@ -109,43 +120,87 @@ export default class LineGraph extends React.Component{
         {name: 'November', appointments: novCount},
         {name: 'December', appointments: decCount}
     ]
-    // console.log('STATE', this.state);
-       return(
-           <div>
-               <div>
-            <LineChart 
-                width={780} 
-                height={250} 
-                data={data}
-                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-                onClick={(e) => {
-                    this.handleClick(e.activeLabel)
-                }}>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip cursor={false} />
-            <Legend 
-                verticalAlign="top" 
-                height={36} 
-                iconType='rect'
-            />
-            <Line 
-                type="monotoneX" 
-                dataKey="appointments" 
-                stroke="#5DADE2" 
-                dot={{ 
-                    stroke: '#5DADE2', 
-                    strokeWidth: .5
-                }}/>
-            </LineChart>
-            </div>
-            <h1>Monthly Appointments History</h1>
-            <h2>{this.state.name}</h2>
-            <ul>
-                {apptDataList}
-            </ul>
-            </div>
-        );
+
+    const apptPercentage = Math.floor((apptInfo.length / totalAppointmentsForUser) * 100 );
+    // console.log('%%', apptPercentage);
+    
+    console.log('STATE', this.state);
+        if (this.state.click === true) {
+            return(
+                <div>
+                    <div>
+                 <LineChart 
+                     width={780} 
+                     height={250} 
+                     data={data}
+                     margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                     onClick={(e) => {
+                         this.handleClick(e.activeLabel)
+                     }}>
+                 <XAxis dataKey="name" />
+                 <YAxis />
+                 <Tooltip cursor={false} />
+                 <Legend 
+                     verticalAlign="top" 
+                     height={36} 
+                     iconType='rect'
+                 />
+                 <Line 
+                     type="monotoneX" 
+                     dataKey="appointments" 
+                     stroke="#5DADE2" 
+                     dot={{ 
+                         stroke: '#5DADE2', 
+                         strokeWidth: .5
+                     }}/>
+                 </LineChart>
+                 </div>
+                 <h1>Monthly Appointments History</h1>
+                 <h2>{this.state.name}</h2>
+                 
+                 <h3>{apptPercentage}% of your appointments were from {this.state.name}</h3> <br />
+     
+                 <ul>
+                     {apptDataList}
+                     {/* {this.handleMessage} */}
+                 </ul>
+                 </div>
+             );
+        } else {
+            return(
+                <div>
+                    <div>
+                 <LineChart 
+                     width={780} 
+                     height={250} 
+                     data={data}
+                     margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                     onClick={(e) => {
+                         this.handleClick(e.activeLabel)
+                     }}>
+                 <XAxis dataKey="name" />
+                 <YAxis />
+                 <Tooltip cursor={false} />
+                 <Legend 
+                     verticalAlign="top" 
+                     height={36} 
+                     iconType='rect'
+                 />
+                 <Line 
+                     type="monotoneX" 
+                     dataKey="appointments" 
+                     stroke="#5DADE2" 
+                     dot={{ 
+                         stroke: '#5DADE2', 
+                         strokeWidth: .5
+                     }}/>
+                 </LineChart>
+                 </div>
+                 
+                 </div>
+             );
+        }
+       
     }
 }
 
