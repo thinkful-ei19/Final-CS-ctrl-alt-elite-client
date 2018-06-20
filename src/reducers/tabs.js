3f7811a76864bb40e1d4b6a84099a5fab4e04f1d
Christopher <cjszk@hotmail.com>
2018-06-20 12:03:12 -0700

    added wallpaper changes for actions/redux

--- src/reducers/tabs.js
+++ src/reducers/tabs.js
@@ -1,8 +1,9 @@
 // import moment from 'moment';
-import { SELECT_TAB } from '../actions/tabs';
+import { SELECT_TAB, SELECT_THEME } from '../actions/tabs';
 
 const initialState = {
-    selectedTab: 'dashboard'
+    selectedTab: 'dashboard',
+    theme: 'default'
 };
 
 export default function reducer(state = initialState, action) {
@@ -10,6 +11,11 @@ export default function reducer(state = initialState, action) {
         return Object.assign({}, state, {
             selectedTab: action.tab
         });
+    } else if (action.type === SELECT_THEME) {
+        return Object.assign({}, state, {
+            theme: action.theme
+        });
     }
+
     return state;
 }
