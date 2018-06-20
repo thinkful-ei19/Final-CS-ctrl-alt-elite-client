549d3edf99f489ececf1e145df62291922abdae2
Julie K <julieskim160@gmail.com>
2018-06-19 17:15:20 -0700

    change calendar button to icon

--- src/components/weekly-view.js
+++ src/components/weekly-view.js
@@ -122,21 +122,24 @@ class WeeklyView extends React.Component {
         return (
             <div className="weekly-view">
                 <div className="weekly-view__weekly">
-                    <button onClick={() => this.toggleCalendar()} className="btn login-button">Monthly</button>                                    
+                    <IconButton aria-label="monthly calendar" onClick={() => this.toggleCalendar()}>
+                        <SvgIcon>
+                        <path d="M20,3h-1V2c0-0.55-0.45-1-1-1h0c-0.55,0-1,0.45-1,1v1H7V2c0-0.55-0.45-1-1-1h0C5.45,1,5,1.45,5,2v1H4C2.9,3,2,3.9,2,5v16
+                        c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V5C22,3.9,21.1,3,20,3z M19,21H5c-0.55,0-1-0.45-1-1V8h16v12C20,20.55,19.55,21,19,21z"/>
+                        </SvgIcon>
+                    </IconButton>                         
                     <div className="weekly-view__weekly__header">
                         <IconButton aria-label="previous week" className="weekly-view__weekly__previous" onClick={decrementWeek}>
                             <SvgIcon>
                             <path xmlns="http://www.w3.org/2000/svg" d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
                             </SvgIcon>
                         </IconButton>
- 
                         <h2 className="weekly-view__weekly__month">{monthYear}</h2>
                         <IconButton aria-label="next week" className="weekly-view__weekly__next" onClick={incrementWeek}>
                             <SvgIcon>
                             <path xmlns="http://www.w3.org/2000/svg" d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
                             </SvgIcon>
                          </IconButton>
-               
                     </div>
                     <WeeklyDays />                                                
                     <ul className="weekly-view__weekly__ul" >
