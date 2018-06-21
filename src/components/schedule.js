fa4dce0e71f2a1acd6f51a775bf04985210245f7
Alexa Scott <arscott4792@gmail.com>
2018-06-21 08:22:20 -0400

    fixing errors in console

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import Paper from '@material-ui/core/Paper';
+// import Paper from '@material-ui/core/Paper';
 import ScheduleList from './schedule-list';
 
 export default function Schedule(props) {
