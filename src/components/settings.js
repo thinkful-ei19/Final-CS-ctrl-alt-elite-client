76c329210ff7b772e6b93687cfb6e37f7ef7bb16
Julie K <julieskim160@gmail.com>
2018-06-26 15:39:24 -0700

    overlay

--- src/components/settings.js
+++ src/components/settings.js
@@ -7,7 +7,7 @@ import { changeTheme } from '../actions/auth';
 import ChangePasswordForm from './change-password';
 
 import lightBackground from '../media/whiteHoriz.jpg';
-import darkBackground from '../media/darkMountain.jpg';
+import darkBackground from '../media/laptop.jpg';
 
 export class Settings extends React.Component {
 
@@ -41,20 +41,22 @@ export class Settings extends React.Component {
             }
         }
         return (
-            <section className="settings" style={ sectionStyle }>
-                <Navigation/>
-                <div className="settings__change-theme">
-                    <h2 className="settings__change-theme__header">Select Theme</h2>
-                    <select onChange={(event) => this.changeTheme(event.target.value)} className="settings__change-theme__drop-down">
-                        <option className="settings__change-theme__option" value="null"></option>
-                        <option className="settings__change-theme__option" value="light">Light</option>
-                        <option className="settings__change-theme__option" value="dark">Dark</option>
-                    </select>
-                </div>
-                <div className="settings__change-password">
-                    <h2 className="settings__change-password__header" >Change Password</h2>
-                    <ChangePasswordForm authToken={this.props.authToken} userId={this.props.currentUser.id} />
-                </div>
+            <section className="settings">
+                <span className="overlay" style={ sectionStyle }>
+                    <Navigation/>
+                    <div className="settings__change-theme">
+                        <h2 className="settings__change-theme__header">Select Theme</h2>
+                        <select onChange={(event) => this.changeTheme(event.target.value)} className="settings__change-theme__drop-down">
+                            <option className="settings__change-theme__option" value="null"></option>
+                            <option className="settings__change-theme__option" value="light">Light</option>
+                            <option className="settings__change-theme__option" value="dark">Dark</option>
+                        </select>
+                    </div>
+                    <div className="settings__change-password">
+                        <h2 className="settings__change-password__header" >Change Password</h2>
+                        <ChangePasswordForm authToken={this.props.authToken} userId={this.props.currentUser.id} />
+                    </div>
+                </span>
             </section>
         )
     }
