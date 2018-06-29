918e27cdf8483a2bd774c17665b7cf5d3722a873
Julie K <julieskim160@gmail.com>
2018-06-28 22:04:43 -0700

    responsiveness

--- src/components/settings.js
+++ src/components/settings.js
@@ -45,12 +45,6 @@ export class Settings extends React.Component {
                 <span className="overlay settings__flex">
                     <Navigation/>
                     <div className="settings__options">
-                        <div className="settings__user">
-                                {/* <SvgIcon>
-                                <path xmlns="http://www.w3.org/2000/svg" d="M12,2C6.48,2,2,6.48,2,12s4.48,10,10,10s10-4.48,10-10S17.52,2,12,2z M12,5c1.66,0,3,1.34,3,3s-1.34,3-3,3S9,9.66,9,8   S10.34,5,12,5z M12,19.2c-2.5,0-4.71-1.28-6-3.22c0.03-1.99,4-3.08,6-3.08c1.99,0,5.97,1.09,6,3.08C16.71,17.92,14.5,19.2,12,19.2z   "/>
-                                </SvgIcon> */}
-                            <h2 className="settings__user__username">Logged in as {this.props.currentUser.username}</h2>
-                        </div>
                         <div className="settings__change-theme">
                             <h2 className="settings__change-theme__header">Select Theme</h2>
                             <select onChange={(event) => this.changeTheme(event.target.value)} className="settings__change-theme__drop-down">
