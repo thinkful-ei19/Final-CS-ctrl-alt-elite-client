2f93ac4e3215f8db96e5d334e0b06495a315034e
Christopher <cjszk@hotmail.com>
2018-06-28 20:28:43 -0700

    landing page done

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -29,7 +29,6 @@ export default function LandingPage(props) {
 
     return(
         <section className="landing-page">
-            
             <nav className="nav">
                <img alt="logo" className="logo" src={Logo}/>
                 <Link to="/login">
@@ -49,43 +48,104 @@ export default function LandingPage(props) {
                     </div>
                 </div>
             </div>
-            <div className="about">            
-                <h2 className="about__header">The perfect business companion</h2>              
-                <div className="about__content">
-                    <ul className="about__list">
-                        <li className="about__list__item">
+            <div className="about">
+                {/* <h2 className="about__header">The perfect business companion</h2>                     */}
+                <ul className="about__list">
+                    <li className="about__list__item">
+                        <span className="about__list__item__left">
                             <h3 className="about__list__item__header">Fast, expressive and convenient</h3>
                             <p className="about__list__item__details"><b className="bold">Schedulr</b> shows your appointments with an interactive calendar 
                                 and allows you full flexibility and control.</p>
-                            <img className="about__list__item__image" src={ssOne}/>
-                            <img className="about__list__item__image" src={ssThree}/>
-                        </li>
-                        <li className="about__list__item">
-                            <h3 className="about__list__item__header">Customization at your fingertips</h3>
+                        </span>
+                        <span className="about__list__item__right">
+                            <img className="about__list__item__image" src={ssOne} />
+                            <img className="about__list__item__image" src={ssThree} />
+                        </span>
+                    </li>
+                    <li className="about__list__item-two"> 
+                        <h3 className="about__list__item__header">Automated reminders</h3>
+                        <p className="about__list__item__details">Let <b className="bold">Schedulr</b> do the tedious work for you. Reminders for clients?
+                        No problem! <b className="bold">Schedulr</b> will do it for you.</p>
+                    </li>
+                    <li className="about__list__item-three">
+                        <div className="about__list__item-three__left">
+                            <img className="about__list__item-three__image about__list__item-three__image__first" src={ssNine} />
+                            <img className="about__list__item-three__image about__list__item-three__image__second" src={ssTwo} />
+                        </div>
+                        <div className="about__list__item-three__right">
+                            <h3 className="about__list__item-three__header">Customization at your fingertips</h3>
+                            <p className="about__list__item-three__details">A truly great user experience is one that can be made personal. 
+                            <b className="bold"> Schedulr</b> offers different themes for a more personalized user experience.</p>
+                        </div>
+                    </li>
+                    <li className="about__list__item-four">
+                        <span className="about__list__item-four__top">
+                            <img className="about__list__item-four__image" src={ssFive} />
+                            <img className="about__list__item-four__image" src={ssSix} />
+                        </span>
+                        <span className="about__list__item-four__bottom">
+                            <h3 className="about__list__item-four__header">Detailed Reports</h3>
+                            <p className="about__list__item-four__details">Good planning isn't just about appointments.
+                            <b className="bold"> Schedulr</b> can show you information about your clientele with its own reports!</p>
+                        </span>
+                    </li>
+                </ul>
+
+                        
+                    {/* <li className="about__list__item">
+                        <span className="about__list__item__left">
+                            
+                            <img className="about__list__item__image" src={ssOne} />
+                        </span>
+                        <span className="about__list__item__right">
+                        <h3 className="about__list__item__header">Customization at your fingertips</h3>
                             <p className="about__list__item__details">A truly great user experience is one that can be made personal. 
                             <b className="bold"> Schedulr</b> offers different themes for a more personalized user experience.</p>
-                            <img className="about__list__item__image about__list__item__overlap" src={ssTwo}/>
-                            <img className="about__list__item__image" src={ssNine}/>
-                        </li>
-                        <li className="about__list__item">
-                            <h3 className="about__list__item__header">Automated reminders</h3>
-                            <p className="about__list__item__details">Let <b className="bold">Schedulr</b> do the tedious work for you. Reminders for clients?
-                                No problem! <b className="bold">Schedulr</b> will do it for you.</p>
-                            <img className="about__list__item__image" src={ssSeven}/>
-                        </li>
-                        <li className="about__list__item">
-                            <h3 className="about__list__item__header">Detailed Reports</h3>
+                        </span>
+                    </li>
+                    <li className="about__list__item">
+                        <span className="about__list__item__left">
+                            <img className="about__list__item__image" src={ssOne} />
+                        </span>
+                        <span className="about__list__item__right">
+                        <h3 className="about__list__item__header">Detailed Reports</h3>
                             <p className="about__list__item__details">Good planning isn't just about appointments.
                             <b className="bold">Schedulr</b> can show you information about your clientele with its own reports!</p>
-                            <img className="about__list__item__image" src={ssFive}/>
-                            <img className="about__list__item__image" src={ssSix}/>
-                        </li>
-
-
-
-                    </ul>
-                </div>
+                        </span>
+                    </li> */}
+               
             </div>
+            {/* <div className="about">            
+                <div className="about__left">
+                    <h2 className="about__header">The perfect business companion</h2>              
+                </div>
+                <div className="about__right">
+                    <div className="about__content">
+                        <ul className="about__list">
+                            <li className="about__list__item">
+                                <h3 className="about__list__item__header">Fast, expressive and convenient</h3>
+                                <p className="about__list__item__details"><b className="bold">Schedulr</b> shows your appointments with an interactive calendar 
+                                    and allows you full flexibility and control.</p>
+                            </li>
+                            <li className="about__list__item">
+                                <h3 className="about__list__item__header">Automated reminders</h3>
+                                <p className="about__list__item__details">Let <b className="bold">Schedulr</b> do the tedious work for you. Reminders for clients?
+                                    No problem! <b className="bold">Schedulr</b> will do it for you.</p>
+                            </li>
+                            <li className="about__list__item">
+                                <h3 className="about__list__item__header">Customization at your fingertips</h3>
+                                <p className="about__list__item__details">A truly great user experience is one that can be made personal. 
+                                <b className="bold"> Schedulr</b> offers different themes for a more personalized user experience.</p>
+                            </li>
+                            <li className="about__list__item">
+                                <h3 className="about__list__item__header">Detailed Reports</h3>
+                                <p className="about__list__item__details">Good planning isn't just about appointments.
+                                <b className="bold">Schedulr</b> can show you information about your clientele with its own reports!</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div> */}
             <div className="whats-new">
                 <h2 className="whats-new__header">Always on the go? Our mobile app has you covered!</h2>
                 <div className="whats-new__left">
